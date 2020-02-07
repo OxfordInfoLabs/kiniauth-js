ab0005c18bbbf3090ff2bd7ca6d5057e5487110f
Mark Robertshaw <mark@oxil.co.uk>
2020-02-07 15:51:05 +0000

    New request params modification

--- ts/util/request-params.ts
+++ ts/util/request-params.ts
@@ -15,7 +15,8 @@ export default class RequestParams {
         var vars = query.split('&');
         for (var i = 0; i < vars.length; i++) {
             var pair = vars[i].split('=');
-            params[pair[0]] = decodeURIComponent(pair[1]);
+            if (pair[1])
+                params[pair[0]] = decodeURIComponent(pair[1].replace(/\+/g, '%20'));
         }
         return params;
 
